028458a126898a61df7fd7db235578839e8f5281
Alan M Silva <alanfortlink@gmail.com>
2022-07-09 13:16:21 +0100

    Make it cleaner

--- game.py
+++ game.py
@@ -7,20 +7,11 @@ from wrapper import Colors, Game
 WIDTH = 800
 HEIGHT = 600
 
-def init(game: Game):
-    # pylint: disable=unused-argument
-    """
-    Initialize the game
-    """
+def init(game):
+    pass
 
-def update(game: Game):
-    # pylint: disable=unused-argument
-    """
-    Game loop
-    """
+def update(game):
+    pass
 
-def draw(game: Game):
-    """
-    Render the game
-    """
-    game.draw_rect(0, 0, 100, 100, Colors.RED)
+def draw(game):
+    game.draw_rect(50, 50, 100, 100, Colors.RED)
